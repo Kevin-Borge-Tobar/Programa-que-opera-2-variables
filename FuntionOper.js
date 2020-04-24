35d35ef4cb9405995163f9d0d54959e13a9ecbfd
Kevin Neftali Borge Tobar <kevinborgetobar1998@gmail.com>
2020-04-23 23:39:16 -0500

    se estableció la función de dividir y sus condiciones.

--- FuntionOper.js
+++ FuntionOper.js
@@ -43,6 +43,25 @@ function subt(){
        valor2 = 0
    }
    /* se realizo una operacion operacion de multiplicación entre las variables*/
-   resultado = (parseFloat(valor1)-parseFloat(valor2));
+   resultado = (parseFloat(valor1)*parseFloat(valor2));
+   document.getElementById("resultado").value = resultado;
+}
+function div(){
+    /* se capturaron los valores de las dos variables a operar para poder analizar este su contenido*/           
+   var valor1 = document.getElementById("valor1").value; 
+   var valor2 = document.getElementById("valor2").value;
+   /* se concionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
+   if(valor1==""){
+       valor1 = 0
+   }
+   if(valor2==""){
+       valor2 = 0
+   }
+   /* se realizo una operacion operacion de div entre las variables*/
+   resultado = (parseFloat(valor1)/parseFloat(valor2));
    document.getElementById("resultado").value = resultado;
  }
+ /* Cada una de estas fuciones se ejecutarán  al momento de hacer click en los diferentes botones y,
+  cada boton  tendra una función especifica, para mostrar el resultado de de la funcion establecida.
+  Y guardarán su resultado en una variable, y este se mostrará al usuario. Como su respuesta.
+ 
