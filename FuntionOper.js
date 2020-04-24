c4d0e00f887cfaab64994de4bdda91569b3a3ec7
Kevin Neftali Borge Tobar <kevinborgetobar1998@gmail.com>
2020-04-23 23:25:52 -0500

    Se establecio la función de multiplicación  y sus condiciones

--- FuntionOper.js
+++ FuntionOper.js
@@ -27,8 +27,22 @@ function subt(){
    if(valor2==""){
        valor2 = 0
    }
-   /* se realizo una operacion operacion de suma entre las variables*/
-   resultado = (parseFloat(valor1)+parseFloat(valor2));
+   /* se realizo una operacion operacion de resta entre las variables*/
+   resultado = (parseFloat(valor1)-parseFloat(valor2));
    document.getElementById("resultado").value = resultado;
  }
- 
+ function mult(){
+    /* se capturaron los valores de las dos variables a operar para poder analizar este su contenido*/           
+   var valor1 = document.getElementById("valor1").value; 
+   var valor2 = document.getElementById("valor2").value;
+   /* se concionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
+   if(valor1==""){
+       valor1 = 0
+   }
+   if(valor2==""){
+       valor2 = 0
+   }
+   /* se realizo una operacion operacion de multiplicación entre las variables*/
+   resultado = (parseFloat(valor1)-parseFloat(valor2));
+   document.getElementById("resultado").value = resultado;
+ }
