67bfcb7c4c25ea9b35273b60e47f34e4331c21a1
Kevin Neftali Borge Tobar <kevinborgetobar1998@gmail.com>
2020-04-24 00:52:41 -0500

    se corrigieron y validaron las operaciones

--- FuntionOper.js
+++ FuntionOper.js
@@ -1,67 +1,73 @@
 'use strict'
-/* se establecion la función de suma*/
+alert(ESTAS A PUNTO DE ABANDONAR TU PAGINA ACTUAL);
 function sum(){
-     /* se capturaron los valores de las dos variables a operar para poder analizar este su contenido*/           
+    /* se capturaron los valores de las dos variables a operar para poder analizar su contenido*/           
     var valor1 = document.getElementById("valor1").value; 
     var valor2 = document.getElementById("valor2").value;
-    /* se concionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
+    /* se condicionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
     if(valor1==""){
         valor1 = 0
     }
     if(valor2==""){
         valor2 = 0
     }
-    /* se realizo una operacion operacion de suma entre las variables*/
+     /* se realizo una operacion operacion de suma entre las variables*/
     resultado = (parseFloat(valor1)+parseFloat(valor2));
     document.getElementById("resultado").value = resultado;
   }
-  /* se establecion la función de resta*/
-function subt(){
-    /* se capturaron los valores de las dos variables a operar para poder analizar este su contenido*/           
-   var valor1 = document.getElementById("valor1").value; 
-   var valor2 = document.getElementById("valor2").value;
-   /* se concionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
-   if(valor1==""){
-       valor1 = 0
-   }
-   if(valor2==""){
-       valor2 = 0
-   }
-   /* se realizo una operacion operacion de resta entre las variables*/
+
+
+  function sub(){
+      /* se capturaron los valores de las dos variables a operar para poder analizar su contenido*/
+    var valor1 = document.getElementById("valor1").value; 
+    var valor2 = document.getElementById("valor2").value;
+    /* se condicionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
+    if(valor1==""){
+        valor1 = 0
+    }
+    if(valor2==""){
+        valor2 = 0
+    }
+    /* se realizo una operacion operacion de resta entre las variables*/
    resultado = (parseFloat(valor1)-parseFloat(valor2));
-   document.getElementById("resultado").value = resultado;
- }
- function mult(){
-    /* se capturaron los valores de las dos variables a operar para poder analizar este su contenido*/           
-   var valor1 = document.getElementById("valor1").value; 
-   var valor2 = document.getElementById("valor2").value;
-   /* se concionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
-   if(valor1==""){
-       valor1 = 0
-   }
-   if(valor2==""){
-       valor2 = 0
-   }
-   /* se realizo una operacion operacion de multiplicación entre las variables*/
-   resultado = (parseFloat(valor1)*parseFloat(valor2));
-   document.getElementById("resultado").value = resultado;
-}
-function div(){
-    /* se capturaron los valores de las dos variables a operar para poder analizar este su contenido*/           
-   var valor1 = document.getElementById("valor1").value; 
-   var valor2 = document.getElementById("valor2").value;
-   /* se concionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
-   if(valor1==""){
-       valor1 = 0
-   }
-   if(valor2==""){
-       valor2 = 0
-   }
-   /* se realizo una operacion operacion de div entre las variables*/
+    document.getElementById("resultado").value = resultado;
+  }
+
+
+  function div(){
+    /* se capturaron los valores de las dos variables a operar para poder analizar su contenido*/ 
+    var valor1 = document.getElementById("valor1").value; 
+    var valor2 = document.getElementById("valor2").value;
+    /* se condicionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
+    if(valor1==""){
+        valor1 = 0
+    }
+    if(valor2==""){
+        valor2 = 0
+    }
+     /* se realizo una operacion operacion de división entre las variables*/
    resultado = (parseFloat(valor1)/parseFloat(valor2));
-   document.getElementById("resultado").value = resultado;
- }
- /* Cada una de estas fuciones se ejecutarán  al momento de hacer click en los diferentes botones y,
+    document.getElementById("resultado").value = resultado;
+  }
+
+
+  function mul(){
+    /* se capturaron los valores de las dos variables a operar para poder analizar su contenido*/ 
+    var valor1 = document.getElementById("valor1").value; 
+    var valor2 = document.getElementById("valor2").value;
+    /* se condicionan las variables para que cuando tengan ningun dato o valor insertado sea igual a 0 */
+    if(valor1==""){
+        valor1 = 0
+    }
+    if(valor2==""){
+        valor2 = 0
+    }
+    /* se realizo una operacion operacion de división entre las variables*/
+   resultado = (parseFloat(valor1)*parseFloat(valor2));
+    document.getElementById("resultado").value = resultado;
+  }    
+  /* Cada una de estas fuciones se ejecutarán  al momento de hacer click en los diferentes botones y,
   cada boton  tendra una función especifica, para mostrar el resultado de de la funcion establecida.
   Y guardarán su resultado en una variable, y este se mostrará al usuario. Como su respuesta.
+  */
  
